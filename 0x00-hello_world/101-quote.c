e7d6adace8806d8478e84784e81931ffa2ffb8ff
Eh1z <godsentehiz11@gmail.com>
2022-09-08 15:03:43 -0700

    I created a git push script for C Hello World and it did this for me

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,23 @@
+#include <unistd.h>
+
 #include <stdio.h>
 
-#include <unistd.h>
+
 
 /**
  *
- * main - Entry point
- *  Return:1
+ * main - Changes stderr without using printf or puts
  *
+ *
+ *
+ * Return: Always 1
  *
  */
 
 int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, ch, sizeof(ch));
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	return (1);
+		return (1);
 }
