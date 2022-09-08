891ebbed5e24583e2063b8132748066b9615dc08
Eh1z <godsentehiz11@gmail.com>
2022-09-08 11:48:46 -0700

    I created a git push script for C Hello World and it did this for me

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-/*
- *
- * main - main block
- *
- * Return: 0
- *
- *   
- */
-int main (void)
-{	
 
-	puts("\"Programming is like building a multilingual puzzle");
-	
-	return(0);
+/**
+ *
+ *  * main - simple example of using puts
+ *
+ *   *
+ *
+ *    * Return: Always 0
+ *
+ *     */
+int main(void)
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
 
 }
