dbbcffda06b84c3907e11c90f402f3edc096efd1
Eh1z <godsentehiz11@gmail.com>
2022-09-09 13:55:25 -0700

    I created a git push script for C variables and it did this for me

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,8 @@
 /**
  * main - main block
  *
- * Description: Get a random number and print the number and if it is positive, negative, or zero
+ * Description: Get a random number and print the number
+ *  and if it is positive, negative, or zero.
  *
  * Return: 0
  *
@@ -21,14 +22,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 		printf("%i is positive\n", n);
 	else if (n < 0)
 		printf("%i is negative\n", n);
 	else
 		printf("%i is zero\n", n);
-	
+
 	return (0);
 
 
