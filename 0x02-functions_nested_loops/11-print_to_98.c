c58137b30bd8d94b0fa02fc148b752922d1e294e
Eh1z <godsentehiz11@gmail.com>
2022-09-13 17:15:01 -0700

    I created a git push script for C nested loops and it did this for me

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all numbers from n to 98
+ * @n: number to start count from
+ *
+ *
+ *
+ *Return: 0
+ *
+ *
+ *
+ *
+ */
+
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+		for ( ; n < 98; n++)
+			printf("%d, ", n);
+	else
+		for ( ; n > 98; n--)
+			printf("%d, ", n);
+	printf("%d\n", n);
+}
